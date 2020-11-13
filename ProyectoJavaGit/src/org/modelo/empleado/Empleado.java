194d9e6657bb89682876f6d5159e831914609de1
nurgl <nurgl@TzeentchLibrary>
2020-11-13 10:38:32 -0600

    // SE CREO EL MODELO EMPLEADO

---
+++ ProyectoJavaGit/src/org/modelo/empleado/Empleado.java
@@ -0,0 +1,44 @@
+package org.modelo.empleado;
+
+public class Empleado 
+{
+	
+	// Variables de la clase
+	private int id;
+	private String nombre;
+	private double salario;
+	
+	
+	// GETTERS AND SETTERS
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	
+	public int getId() 
+	{
+		return id;
+	}
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
+	
+	public String getNombre() 
+	{
+		return nombre;
+	}
+	public void setNombre(String nombre) 
+	{
+		this.nombre = nombre;
+	}
+	
+	public double getSalario() 
+	{
+		return salario;
+	}
+	public void setSalario(double salario) 
+	{
+		this.salario = salario;
+	}
+	
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	
+}
